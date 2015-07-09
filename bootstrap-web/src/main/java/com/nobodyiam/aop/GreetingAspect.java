5246d4423f9ab92543e5bf599591ca59926ca9d6
nobodyiam <nobodyiam@gmail.com>
2015-07-09 23:41:33 +0800

    update

--- bootstrap-web/src/main/java/com/nobodyiam/aop/GreetingAspect.java
+++ bootstrap-web/src/main/java/com/nobodyiam/aop/GreetingAspect.java
@@ -2,12 +2,11 @@ package com.nobodyiam.aop;
 
 import com.nobodyiam.dto.Greeting;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * Created by Jason on 7/9/15.
@@ -17,25 +16,42 @@ import org.springframework.stereotype.Component;
 public class GreetingAspect {
     private static final Logger logger = LoggerFactory.getLogger(GreetingAspect.class);
 
-    @Pointcut("execution(* com.nobodyiam.api.GreetingService.insertGreeting(..))")
-    public void insertGreeting() {}
+    @Pointcut("execution(* com.nobodyiam.api.GreetingService.insertGreeting(..)) && args(greeting)")
+    public void insertGreeting(Greeting greeting) {
+    }
 
-    @Around("insertGreeting()")
-    public int logCreatedGreetingId(ProceedingJoinPoint joinPoint) throws Throwable {
-        int insertedRows = (Integer)joinPoint.proceed();
-        if (logger.isDebugEnabled()) {
-            Object[] args = joinPoint.getArgs();
-            String greetingContent = "";
-            long greetingId = 0;
-            if (args != null && args.length > 0 && args[0] instanceof Greeting) {
-                greetingContent = ((Greeting) args[0]).getContent();
-                greetingId = ((Greeting) args[0]).getId();
-            }
-            logger.debug(String.format("Creating greeting with content: %s, new greeting id is: %d", greetingContent, greetingId));
-        }
+    @Pointcut("execution(* com.nobodyiam.api.GreetingService.updateGreeting(..)) && args(greeting)")
+    public void updateGreeting(Greeting greeting) {
+    }
+
+    @Pointcut("execution(* com.nobodyiam.api.GreetingService.getGreetings(..)) && args(limit, offset)")
+    public void getGreetings(int limit, int offset) {
+    }
+
+    @Pointcut("execution(* com.nobodyiam.api.GreetingService.getGreeting(..))")
+    public void getGreeting() {}
+
+    @Around(value = "insertGreeting(greeting)", argNames = "joinPoint, greeting")
+    public int logCreatedGreetingId(ProceedingJoinPoint joinPoint, Greeting greeting) throws Throwable {
+        logger.debug(String.format("Creating greeting with content: %s", greeting.getContent()));
+        int insertedRows = (Integer) joinPoint.proceed();
+        logger.debug(String.format("Greeting created - id: %d, content: %s", greeting.getId(), greeting.getContent()));
 
         return insertedRows;
     }
 
+    @Before(value = "updateGreeting(greeting)", argNames = "greeting")
+    public void beforeUpdatingGreeting(Greeting greeting) {
+        logger.debug(String.format("Updating greeting(id=%d) with content: %s", greeting.getId(), greeting.getContent()));
+    }
 
+    @AfterThrowing(value = "getGreetings(limit, offset)", argNames = "limit, offset, e", throwing = "e")
+    public void logInvalidGreetingsBatch(int limit, int offset, Exception e) {
+        logger.error(String.format("Invalid batch when querying greetings - limit: %d, offset: %d, exception: %s", limit, offset, e));
+    }
+
+    @After("getGreeting()")
+    public void logAfterGetGreeting() {
+        logger.debug("Greeting queried...");
+    }
 }
