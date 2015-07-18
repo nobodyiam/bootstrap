64dbe8a73e1c86b9f149f20800697a4a3d241c19
nobodyiam <nobodyiam@gmail.com>
2015-07-19 00:39:56 +0800

    add cancel action

--- bootstrap-web/src/main/webapp/s/scripts/app.js
+++ bootstrap-web/src/main/webapp/s/scripts/app.js
@@ -5,6 +5,8 @@
         'httpInterceptors' //custom http interceptor
     ]);
 
+    var GREETINGS_PATH = '/greetings';
+
     app.controller('GreetingsController', function ($scope, $http, $modal, toastr) {
         this.pageNo = 1;
         this.pageSize = 10;
@@ -12,7 +14,7 @@
         var self = this;
 
         this.getGreetings = function (pageNo, pageSize) {
-            $http.get('/greetings', {
+            $http.get(GREETINGS_PATH, {
                 data: {
                     offset: (pageNo - 1) * pageSize,
                     limit: pageSize
@@ -23,20 +25,10 @@
             }).error(function (data, status, headers, config) {
                 toastr.error((data && data.msg) || 'Loading failed');
             })
-        }
+        };
 
         this.add = function () {
-            var modalInstance = $modal.open({
-                templateUrl: 'templates/edit.html',
-                controller: 'GreetingController',
-                controllerAs: 'greetingCtrl',
-                size: 'lg',
-                resolve: {
-                    greeting: function () {
-                        return {};
-                    }
-                }
-            });
+            var modalInstance = this.popUpEditModal({});
 
             modalInstance.result.then(function (addedItem) {
                 if (self.pageNo == 1 && self.greetings.length < self.pageSize) {
@@ -46,17 +38,7 @@
         };
 
         this.edit = function (greeting) {
-            var modalInstance = $modal.open({
-                templateUrl: 'templates/edit.html',
-                controller: 'GreetingController',
-                controllerAs: 'greetingCtrl',
-                size: 'lg',
-                resolve: {
-                    greeting: function () {
-                        return angular.copy(greeting);
-                    }
-                }
-            });
+            var modalInstance = this.popUpEditModal(greeting);
 
             modalInstance.result.then(function (updatedItem) {
                 var matchingIndex = self.findGreetingIndex(updatedItem);
@@ -68,12 +50,28 @@
             });
         };
 
+        this.popUpEditModal = function (greeting) {
+            return $modal.open({
+                templateUrl: 'templates/edit.html',
+                controller: 'GreetingController',
+                controllerAs: 'greetingCtrl',
+                size: 'lg',
+                backdrop: 'static',
+                keyboard: false,
+                resolve: {
+                    greeting: function () {
+                        return angular.copy(greeting);
+                    }
+                }
+            });
+        };
+
         this.delete = function (greeting) {
             if (!confirm('Are you sure to delete this greeting?')) {
                 return;
             }
 
-            $http.delete('/greetings/' + greeting.id).success(function (data, status, headers, config) {
+            $http.delete(GREETINGS_PATH + '/' + greeting.id).success(function (data, status, headers, config) {
                 toastr.success('Deleted successfully');
 
                 var matchingIndex = self.findGreetingIndex(data);
@@ -92,8 +90,7 @@
             return _.findIndex(self.greetings, function (item) {
                 return item.id == greeting.id;
             });
-        }
-
+        };
 
         this.init = function () {
             this.getGreetings(this.pageNo, this.pageSize);
@@ -105,25 +102,34 @@
     app.controller('GreetingController', function ($scope, $http, $modalInstance, toastr, greeting) {
         this.item = greeting;
         var self = this;
+        var requesting = false;
 
         this.save = function () {
-            this.item.id ? this.update() : this.create();
+            this.requesting = true;
+            var finallyCallback = function() {
+               self.requesting = false;
+            };
+            this.item.id ? this.update(finallyCallback) : this.create(finallyCallback);
         };
 
-        this.create = function () {
-            $http.post('/greetings', this.item).success(function (data, status, headers, config) {
+        this.create = function (finallyCallback) {
+            $http.post(GREETINGS_PATH, this.item).success(function (data, status, headers, config) {
+                finallyCallback && finallyCallback.call();
                 toastr.success('Saved successfully');
                 self.close(data);
             }).error(function (data, status, headers, config) {
+                finallyCallback && finallyCallback.call();
                 toastr.error((data && data.msg) || 'Saved failed');
             })
         };
 
-        this.update = function () {
-            $http.put('/greetings/' + this.item.id, this.item).success(function (data, status, headers, config) {
+        this.update = function (finallyCallback) {
+            $http.put(GREETINGS_PATH + '/' + this.item.id, this.item).success(function (data, status, headers, config) {
+                finallyCallback && finallyCallback.call();
                 toastr.success('Updated successfully');
                 self.close(data);
             }).error(function (data, status, headers, config) {
+                finallyCallback && finallyCallback.call();
                 toastr.error((data && data.msg) || 'Updated failed');
             })
         };
@@ -131,6 +137,15 @@
         this.close = function (data) {
             $modalInstance.close(data);
         };
+
+        this.cancel = function () {
+            if (this.requesting) {
+                return;
+            }
+            if ($scope.greetingForm.$pristine || confirm('Changes not saved, are you sure to give up?')) {
+                $modalInstance.dismiss('cancel');
+            }
+        }
     });
 
 })(jQuery);
