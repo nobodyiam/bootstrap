3d9da07d57d76ff50a025cb1daaa924d1ebe38e0
nobodyiam <nobodyiam@gmail.com>
2015-07-19 16:12:25 +0800

    exclude filter config from component scan

--- bootstrap-web/src/main/java/com/nobodyiam/config/AppConfig.java
+++ bootstrap-web/src/main/java/com/nobodyiam/config/AppConfig.java
@@ -3,12 +3,14 @@ package com.nobodyiam.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * Created by Jason on 7/5/15.
  */
 @Configuration
 @EnableAspectJAutoProxy
-@ComponentScan("com.nobodyiam")
+@ComponentScan(value = "com.nobodyiam",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = FilterConfig.class))
 public class AppConfig {
 }
