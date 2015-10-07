0863a3f0d71756e623ae9d9837c5b185ee782bac
nobodyiam <nobodyiam@gmail.com>
2015-10-07 14:25:28 +0800

    replace jackson with gson

--- bootstrap-web/src/main/java/com/nobodyiam/config/WebConfig.java
+++ bootstrap-web/src/main/java/com/nobodyiam/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.nobodyiam.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -26,7 +27,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter()); // must add this to support @ResponseBody
+//        converters.add(new MappingJackson2HttpMessageConverter()); // must add this to support @ResponseBody
+        converters.add(new GsonHttpMessageConverter()); // must add this to support @ResponseBody
     }
 
     @Bean
